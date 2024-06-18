3a9057916d6aa85b1e89bd20bd6ecd3d54d44673
Tom Lasseret <tlassere@k1r1p7.42mulhouse.fr>
2024-06-18 15:21:54 +0200

    Feat: change intern class

--- ex03/Intern.hpp
+++ ex03/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 23:22:05 by tlassere          #+#    #+#             */
-/*   Updated: 2024/05/26 01:20:29 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/06/18 15:19:51 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,12 @@ class	Intern
 		ShrubberyCreationForm	shru;
 		AForm	*tab[SIZE_FORM];
 
+		Intern( Intern const& cpy );
+		Intern&	operator=( Intern const& cpy );
+
 	public:
 		Intern(void);
 		~Intern(void);
-		Intern( Intern const& cpy );
-		Intern&	operator=( Intern const& cpy );
 
 		AForm	*makeForm(std::string const& name,
 			std::string const& target) const;
